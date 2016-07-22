d3586eddfb05f676c808070dc72ab119faf8fa2b
User <User@LAPTOP-BEM5NFH2>
2016-07-22 17:13:43 -0500

    XAVIER

--- SistemaVentas/src/interfaces/conexion.java
+++ SistemaVentas/src/interfaces/conexion.java
@@ -6,10 +6,44 @@
 
 package interfaces;
 
-/**
- *
- * @author Geovanny
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
+
 public class conexion {
     
-}
+    public conexion() {
+    }
+    public Statement conn(){
+        try{  
+//        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        System.out.println("Conectando con la base de datos...");
+        Connection connection = DriverManager.getConnection(
+        "jdbc:oracle:thin:@localhost:1521:XE");
+        Statement statement = connection.createStatement();
+        return statement;
+        }catch(Exception e){
+        System.out.println("The exception raised is:" + e);
+        return null;  
+        } 
+    }
+    public LinkedList<String> query (String from){
+            try{
+            Statement st = conn();
+            ResultSet resultSet = st.executeQuery(from);
+
+            LinkedList<String> result = new LinkedList();
+            while(resultSet.next()){
+            for(int i=1;i< resultSet.getMetaData().getColumnCount(); i++){
+            result.add(resultSet.getString(i));
+            }
+            }
+            return result;
+            }catch (Exception e){
+            return null;
+            }
+    }
+    }
